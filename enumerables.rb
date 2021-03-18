f64e67d0ae3f1c9ae9cfaf11083262cae4118934
nikos@pountzas.gr <72627495+pountzas@users.noreply.github.com>
2021-03-18 17:20:26 +0200

    add my_each and my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,17 @@
-puts 'Building Enumerable Projects'
+module Enumerable
+  def my_each(array)
+    array.length.times do |i|
+      yield(array[i])
+    end
+  end
+
+  def my_each_with_index(array)
+    array.length.times do |i|
+      yield(array[i], i)
+    end
+  end
+end
+
+array = %w[asd asdd qweee]
+array.my_each(array) { |item| puts item }
+array.my_each_with_index(array) { |item, index| puts "asdasd #{item}" }
